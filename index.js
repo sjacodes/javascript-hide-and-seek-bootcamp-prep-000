59ba90372132da5f5130b0ad9187baba9f43ff6e
Sarah Jacob <smjacob91@gmail.com>
2018-06-25 13:19:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,7 @@ function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.rankedLists')
   
   for (let i = 0; i < rankedLists.length; i++) {
-    
+    I DONT KNOW WHAT TO DO NOW - 
   }
 }
   
-  
-  
-  
-  function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-    }
-  }
-}
